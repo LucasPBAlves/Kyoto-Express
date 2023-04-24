6bd1a38a8ff67fcce06e59bc7a23019d86acef5c
Scrow-rgb <78487065+Scrow-rgb@users.noreply.github.com>
2023-04-24 16:53:38 -0300

    Update Cadastro.dart

--- lib/Cadastro.dart
+++ lib/Cadastro.dart
@@ -4,12 +4,12 @@ class CadastroPage extends StatefulWidget {
   const CadastroPage({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _CadastroPageState createState() => _CadastroPageState();
 }
 
 class _CadastroPageState extends State<CadastroPage> {
   final _formKey = GlobalKey<FormState>();
+  bool preencheu = false;
 
   @override
   Widget build(BuildContext context) {
@@ -38,106 +38,120 @@ class _CadastroPageState extends State<CadastroPage> {
                         fillColor: Colors.white24,
                         labelText: 'Nome',
                         labelStyle: TextStyle(color: Colors.white),
-                        errorStyle: TextStyle(color: Colors.white),
                       ),
                       validator: (value) {
                         if (value?.isEmpty ?? true) {
-                          return 'Por favor, insira um nome';
+                          return '';
                         }
                         return null;
                       },
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 10.0),
                     TextFormField(
                       decoration: const InputDecoration(
                         filled: true,
                         fillColor: Colors.white24,
                         labelText: 'Email',
                         labelStyle: TextStyle(color: Colors.white),
-                        errorStyle: TextStyle(color: Colors.white),
                       ),
                       validator: (value) {
                         if (value!.isEmpty) {
-                          return 'Por favor, insira um email';
+                          return '';
                         }
                         return null;
                       },
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 10.0),
                     TextFormField(
                       decoration: const InputDecoration(
                         filled: true,
                         fillColor: Colors.white24,
                         labelText: 'Telefone',
                         labelStyle: TextStyle(color: Colors.white),
-                        errorStyle: TextStyle(color: Colors.white),
                       ),
                       validator: (value) {
                         if (value?.isEmpty ?? true) {
-                          return 'Por favor, insira um número de telefone';
+                          return '';
                         }
                         return null;
                       },
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 10.0),
                     TextFormField(
                       decoration: const InputDecoration(
                         filled: true,
                         fillColor: Colors.white24,
                         labelText: 'CPF',
                         labelStyle: TextStyle(color: Colors.white),
-                        errorStyle: TextStyle(color: Colors.white),
                       ),
                       validator: (value) {
                         if (value?.isEmpty ?? true) {
-                          return 'Por favor, insira um CPF';
+                          return '';
                         }
                         return null;
                       },
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 10.0),
                     TextFormField(
                       decoration: const InputDecoration(
                         filled: true,
                         fillColor: Colors.white24,
                         labelText: 'Endereço',
                         labelStyle: TextStyle(color: Colors.white),
-                        errorStyle: TextStyle(color: Colors.white),
                       ),
-
+                      validator: (value) {
+                        if (value?.isEmpty ?? true) {
+                          return '';
+                        }
+                        return null;
+                      },
                     ),
-                    const SizedBox(height: 16.0),
+                    const SizedBox(height: 10.0),
                     TextFormField(
                       decoration: const InputDecoration(
                         filled: true,
                         fillColor: Colors.white24,
                         labelText: 'Senha',
                         labelStyle: TextStyle(color: Colors.white),
-                        errorStyle: TextStyle(color: Colors.white),
                       ),
                       obscureText: true,
                       validator: (value) {
                         if (value?.isEmpty ?? true) {
-                          return 'Por favor, insira uma senha';
+                          return '';
                         }
                         return null;
                       },
                     ),
                     const SizedBox(height: 20.0),
-
-                    const SizedBox(height: 20.0),
                     TextButton(
                       onPressed: () {
                         if (_formKey.currentState!.validate()) {
-                          // Ações a serem executadas quando o formulário for validado
+                          setState(() {
+                            preencheu = true;
+                          });
+                          Navigator.pushNamed(context, "/Verificar");
+                        } else {
+                          setState(() {
+                            preencheu = false;
+                          });
+                          showDialog(
+                              context: context,
+                              builder: (_) => AlertDialog(
+                                      title: Text("Precisa preencher os dados"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                          child: Text("Ok"),
+                                        )
+                                      ]));
                         }
-
                       },
                       style: ButtonStyle(
                         foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.red),
+                            MaterialStateProperty.all<Color>(Colors.red),
                         backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.white),
+                            MaterialStateProperty.all<Color>(Colors.white),
                       ),
                       child: const Text('Próximo'),
                     ),
